fe9a8ac4136b54e721e6cb2191ab8038802c5f12
Travis Cunningham <travis@travis-cunningham.com>
2017-11-17 07:59:38 -0800

    adding pods to source control

--- swiftchat/ViewController.swift
+++ swiftchat/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func loginButton_click(_ sender: Any) {
+        self.performSegue(withIdentifier: "showProfile", sender: sender)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
